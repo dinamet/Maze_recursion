c6dc89345def3e8b5f9e2ce2ce9b700af6f93513
Work <nurdin.ametov@alatoo.edu.kg>
2025-04-14 00:42:00 +0600

    3

--- Maze.java
+++ Maze.java
@@ -29,6 +29,26 @@ public class Maze {
         carve(1, 1);
     }
 
+    private static void carve(int x, int y) {
+        int[] dx = {0, 0, 2, -2};
+        int[] dy = {-2, 2, 0, 0};
+
+        maze[x][y] = ' ';
+
+        Integer[] directions = {0, 1, 2, 3};
+        Collections.shuffle(Arrays.asList(directions), rand);
+
+        for (int dir : directions) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            if (nx > 0 && ny > 0 && nx < SIZE - 1 && ny < SIZE - 1 && maze[nx][ny] == '█') {
+                maze[x + dx[dir] / 2][y + dy[dir] / 2] = ' ';
+                carve(nx, ny);
+            }
+        }
+    }
+
     private static boolean solve(int x, int y) {
         // recursive pathfinding
         return false;
